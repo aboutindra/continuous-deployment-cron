1bd24e4ba277958321113ba58c7eefe47d087b97
aboutindracom <me@aboutindra.com>
2020-11-04 21:45:36 +0700

    adding interval

--- automated-pull-n-runnning.sh
+++ automated-pull-n-runnning.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-for repo in Kotakjualan-Email  pemilo-admin  pemilo-candidate  pemilo-room  pemilo-vote  sqola-option  sqola-student pemilo-fadmin pemilo-fclient; do
+while true; do
+
+  for repo in Kotakjualan-Email  pemilo-admin  pemilo-candidate  pemilo-room  pemilo-vote  sqola-option  sqola-student pemilo-fadmin pemilo-fclient; do
     (
-	cd "${repo}"
-	
-	changed=0
-        git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
-        
-	if [ $changed = 1 ]; then
-       		git pull	
-		pm2 restart "${repo}"
-        	echo "${repo} service been updated" + " ( " + $(date) +" )";
-	else
-    		echo "${repo} Up-to-date"
-	fi	
-    
-    )	
+      cd "${repo}"
+
+      changed=0
+      git remote update && git status -uno | grep -q 'Your branch is behind' && changed=1
+
+      if [ $changed = 1 ]; then
+              git pull
+        pm2 restart "${repo}"
+              echo "${repo} service been updated" + " ( " + $(date) +" )";
+      else
+            echo "${repo} Up-to-date"
+      fi
+    )
+  done
+
+  sleep 60
 done
+
